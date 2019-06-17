866d29f44c213bf846b01f26d3e354ea692e55a3
Mengti Sun <mengtisun@berkeley.edu>
2019-06-17 14:59:36 +0800

    new

--- model/train_cnn.py
+++ model/train_cnn.py
@@ -65,8 +65,8 @@ if __name__ == "__main__":
     target_column = config.conf['model_params']['target_column']
     selected_columns = config.conf['model_params']['selected_columns']
     pred_dim = config.conf['model_params']['pred_dim']
-    #use_cuda = config.conf['model_params']['pred_use_cuda']
-    use_cuda= False
+    # use_cuda = config.conf['model_params']['pred_use_cuda']
+    use_cuda = False
     # lr = config.conf['model_params']['lr']
     lr = 0.001
     # n_epochs = config.conf['model_params']['epochs']
@@ -76,7 +76,6 @@ if __name__ == "__main__":
     n_test = pred_dim
     # for data split
 
-
     # 载入训练样本和目标数据集
     train_samples_dict = build_train_samples_dict()
     train_targets_arr = build_train_targets_array()
@@ -84,13 +83,11 @@ if __name__ == "__main__":
     # 构建训练和验证数据集
     trainloader, verifyloader, X_train, y_train, X_verify, y_verify = build_train_and_verify_datasets()
 
-
     # 定义模型
     model = models.ConvNet()
     input_size = X_train.shape[2]
     alpha_layer = models.AlphaLayer(input_size)
     weights_layer = models.WeightsLayer(X_train.shape[1], y_train.shape[1])
-
 
     # 设定优化器
     criterion = nn.MSELoss()
@@ -99,13 +96,14 @@ if __name__ == "__main__":
     # Train the model
     total_step = len(trainloader)
     loss_list = []
+    verify_loss_record = []
     acc_list = []
     for epoch in range(n_epochs):
         for i, (train_x, train_y) in enumerate(trainloader):
             # Run the forward pass
             train_x = train_x.unsqueeze(1)
             train_out = model(train_x)
-            #train_out = outputs[:, -pred_dim:, 0]
+            # train_out = outputs[:, -pred_dim:, 0]
             train_y = train_y[:, :10, :]
             print(train_out.shape, train_y.shape)
 
@@ -117,59 +115,22 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
 
-            # Track the accuracy
-            total = train_out.size()
-            _, predicted = torch.max(train_out.data, 1)
-            correct = (predicted == np.long(train_y)).sum().item()
-            acc_list.append(correct / total)
+            # 准确率
+            # total = train_out.size()
+            # _, predicted = torch.max(train_out.data, 1)
+            # correct = (predicted == np.long(train_y)).sum().item()
+            # acc_list.append(correct / total)
 
-            if (i + 1) % 100 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-                      .format(epoch + 1, n_epochs, i + 1, total_step, loss.item(),
-                              (correct / total) * 100))
+        # 验证集
+        with torch.no_grad():
+            for verify_x, verify_y in verifyloader:
+                cnn_verify_out = model(verify_x)
+                cnn_verify_out = cnn_verify_out[:, -pred_dim:, 0]
+                verify_loss = criterion(cnn_verify_out, verify_y[:, :, 0])
+            verify_loss_record.append(verify_loss)
 
-    # # Train the model
-    # total_step = len(trainloader)
-    # verify_loss_record = []
-    # train_loss_record = []
-    # acc_list = []
-    # for epoch in range(n_epochs):
-    #     # 训练集
-    #     for train_x, train_y in trainloader:
-    #         input_weights = alpha_layer(train_x)[:, :, 0]
-    #         output_weights = weights_layer(input_weights)
-    #         train_x = train_x.unsqueeze(1)
-    #         print("train_x: ", train_x.shape)
-    #         train_out = model(train_x)
-    #         #train_out = train_out[:, -pred_dim:, 0]
-    #         print(train_out.shape, output_weights.shape)
-    #         train_loss = criterion(torch.mul(train_out, output_weights), train_y[:, :, 0])
-    #         optimizer.zero_grad()
-    #         train_loss.backward()
-    #         optimizer.step()
-    #     train_loss_record.append(train_loss)
-    #
-    #     # 验证集
-    #     with torch.no_grad():
-    #         for verify_x, verify_y in verifyloader:
-    #             input_weights = models.alpha_layer(verify_x)[:, :, 0]
-    #             output_weights = models.weights_layer(input_weights)
-    #             cnn_verify_out = model(verify_x)
-    #             cnn_verify_out = cnn_verify_out[:, -pred_dim:, 0]
-    #             verify_loss = criterion(torch.mul(cnn_verify_out, output_weights), verify_y[:, :, 0])
-    #         verify_loss_record.append(verify_loss)
-    # 测试模型
-    # model.eval()
-    # with torch.no_grad():
-    #     correct = 0
-    #     total = 0
-    #     for images, labels in test_loader:
-    #         outputs = model(images)
-    #         _, predicted = torch.max(outputs.data, 1)
-    #         total += labels.size(0)
-    #         correct += (predicted == labels).sum().item()
-    #
-    #     print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
+        if epoch % 5 == 0:
+            print(epoch, loss, verify_loss)
 
     # #保存模型
     # with open('../tmp/model_struc_params_cnn.pkl', 'w') as f:
