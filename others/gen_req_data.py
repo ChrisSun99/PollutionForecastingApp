560195a29cb1aa443dbbe2bfd953d64448a01ea7
dreisteine262 <luolei@rockontrol.com>
2019-06-21 10:50:27 +0800

    生成模拟请求json数据

---
+++ others/gen_req_data.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 14:32:52 2018
+
+@author: luolei
+
+生成请求数据
+"""
+import json
+
+
+def gen_req_data(start_time, end_time = ''):
+	"""
+	生成请求数据
+	:param start_time: str, 查询起始时间, 如'2017010101'
+	:param end_time: str, 查询结束时间, 如'2017010123'
+	:return: req_dict
+	"""
+	req_dict = {
+		'start_time': start_time,
+		'end_time': end_time
+	}
+	req_data = json.dumps(req_dict)
+	return req_data
+
+
+if __name__ == '__main__':
+	start_time = '2017010101'
+	req_data = gen_req_data(start_time)
+	
+	
+	
+
