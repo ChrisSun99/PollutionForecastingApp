74e409f98cfbd82ac64c62c48db44ae5188bb922
dreisteine262 <luolei@rockontrol.com>
2019-06-21 10:51:00 +0800

    根据请求数据，从db中拉取相应数据并整理为待分析数据表

--- mods/build_samples_and_targets.py
+++ mods/build_samples_and_targets.py
@@ -7,9 +7,6 @@ Created on 2019/6/7 19:14
 """
 import time
 import pandas as pd
-import numpy as np
-import copy
-from scipy.ndimage.interpolation import shift
 import category_encoders as ce
 import sys
 
@@ -20,34 +17,40 @@ from mods.pull_data_from_db import pull_data_from_db
 from mods.config_loader import config
 
 
-def build_data_frame_for_correlation_analysis():
-    """构建相关分析用样本"""
-    db_config = config.conf['db_config']
-    categorical_columns = config.conf['model_params']['categorical_columns']
-    numerical_columns = config.conf['model_params']['numerical_columns']
-    
-    # 载入数据
-    data = pull_data_from_db(db_config)
-
-    # 时间戳
-    data['time_stamp'] = data.loc[:, 'ptime'].apply(lambda x: int(time.mktime(time.strptime(str(int(float(x))), "%Y%m%d%H"))))
-
-    # 分离类别数据
-    data = data.drop(["_class", "_id", "city", "itime", "regionId"], axis = 1)
-    data = data[['time_stamp'] + categorical_columns + numerical_columns]
-
-    # 对类别数据进行One-Hot Encoding
-    enc = ce.OneHotEncoder(return_df = True, handle_unknown = "ignore")
-    encoded_categorical_data = pd.DataFrame(enc.fit_transform(data[categorical_columns]))
-    
-    # 对数值数据进行滤波
-    filtered_numerical_data = savitzky_golay_filtering(data[numerical_columns].astype('float'))
-    
-    # 合并数据
-    data = pd.concat([data[['time_stamp']], filtered_numerical_data, encoded_categorical_data], axis = 1, sort = False)
-
-    return data
+def build_data_frame_for_correlation_analysis(start_time, end_time):
+	"""
+	构建相关分析用样本
+	:param start_time: str, 查询起始时间, 如'2017010101'
+	:param end_time: str, 查询结束时间, 如'2017010123'
+	:return:
+	"""
+	db_config = config.conf['db_config']
+	categorical_columns = config.conf['model_params']['categorical_columns']
+	numerical_columns = config.conf['model_params']['numerical_columns']
+	
+	# 载入数据
+	data = pull_data_from_db(db_config, start_time, end_time)
+	
+	# 时间戳
+	data['time_stamp'] = data.loc[:, 'ptime'].apply(lambda x: int(time.mktime(time.strptime(str(int(float(x))), "%Y%m%d%H"))))
+	
+	# 分离类别数据
+	data = data[['ptime', 'time_stamp'] + categorical_columns + numerical_columns]
+	
+	# 对类别数据进行One-Hot Encoding
+	enc = ce.OneHotEncoder(return_df = True, handle_unknown = "ignore")
+	encoded_categorical_data = pd.DataFrame(enc.fit_transform(data[categorical_columns]))
+	
+	# 对数值数据进行滤波
+	filtered_numerical_data = savitzky_golay_filtering(data[numerical_columns].astype('float'))
+	
+	# 合并数据
+	data = pd.concat([data[['ptime', 'time_stamp']], filtered_numerical_data, encoded_categorical_data], axis = 1, sort = False)
+	
+	return data
 
 
 if __name__ == '__main__':
-    data = build_data_frame_for_correlation_analysis()
+	start_time = '2017010101'
+	end_time = '2017123123'
+	data = build_data_frame_for_correlation_analysis(start_time, end_time)
--- mods/pull_data_from_db.py
+++ mods/pull_data_from_db.py
@@ -1,12 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2019/6/7 19:14
+@author: luolei
 
+从数据库拉取数据
+"""
+import sys
 import pymysql
 import pandas as pd
 
+sys.path.append('../')
 pymysql.install_as_MySQLdb()
 
+from mods.config_loader import config
 
-def pull_data_from_db(db_config):
+
+def pull_data_from_db(db_config, start_time, end_time = None):
     """从数据库中拉取原始数据，转为数据表"""
+    # Build connection to db.
     db = pymysql.connect(
         host = db_config['host'],            # your host
         user = db_config['user'],            # username
@@ -15,11 +26,29 @@ def pull_data_from_db(db_config):
     
     # Create a Cursor object to execute queries.
     cursor = db.cursor(pymysql.cursors.DictCursor)
-    cursor.execute("SELECT * FROM taiyuan_cityHour")
+    if end_time is None:
+        cursor.execute("""
+        SELECT * FROM taiyuan_cityHour WHERE ptime >= {}
+        """.format(start_time))
+    else:
+        cursor.execute("""
+        SELECT * FROM taiyuan_cityHour WHERE ptime >= {} AND ptime <= {}
+        """.format(start_time, end_time))
     
-    # cursor.execute("SELECT {}, pm25, {} FROM taiyuan_cityHour".format(feature1, feature2))
+    # Fetch data.
     result_set = cursor.fetchall()
     cursor.close()
     result = pd.DataFrame(list(result_set))
     
     return result
+
+
+if __name__ == '__main__':
+    # Load db config.
+    db_config = config.conf['db_config']
+    
+    # Result without end time.
+    result_without_end = pull_data_from_db(db_config, '2016050603')
+
+    # Result with end time.
+    result_with_end = pull_data_from_db(db_config, '2016050603', '2016050723')
